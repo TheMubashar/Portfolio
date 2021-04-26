cccee82097ad982baf945c9e84a722088b6996c2
TheMubashar <muby945@gmail.com>
2021-04-26 15:19:43 +0500

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,384 @@
+﻿// Summary And Greeting Section
+import emoji from "react-easy-emoji";
+
+const illustration = {
+  animated: true // set to false to use static SVG
+};
+
+const greeting = {
+  username: "Mubashar Hussain",
+  title: "Hi all, I'm Mubashar",
+  subTitle: emoji(
+    "A passionate Full Stack Software Developer 🚀 having an experience of designing and building Web applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks."
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/1R2a9Rp4OqcAF3f_1XI-XRAx2Bxio-B6R/view?usp=sharing",
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/TheMubashar",
+  linkedin: "https://www.linkedin.com/in/mubashar-hussain-a2934a147/",
+  outlook: "mubasharhussain404@outlook.com",
+  // gitlab: "https://gitlab.com/",
+  // facebook: "https://www.facebook.com/",
+  // medium: "https://medium.com/@davidrakosi",
+  // stackoverflow: "https://stackoverflow.com/users/13263979/drakosi",
+  instagram: 'https://www.instagram.com/themubashar.hussain/',
+  // twitter: 'https://twitter.com/davidrakosi_',
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web applications"
+    ),
+    // emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji(
+      "⚡ Integration of third party services such as Firebase"
+    )
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "fab fa-sass"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+    // {
+    //   skillName: "swift",
+    //   fontAwesomeClassname: "fab fa-swift"
+    // },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire"
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "Beaconhouse National University",
+      logo: require("./assets/images/BNU-Logo.png"),
+      subHeader: "Bachelor of Software Engineering",
+      duration: "September 2016 - June 2020",
+      desc: "Participated in the research of XXX and published 3 papers.",
+      descBullets: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      ]
+    },
+    {
+      schoolName: "Beaconhouse School System",
+      logo: require("./assets/images/Bss-Logo.jpg"),
+      subHeader: "A-level",
+      duration: "September 2014 - June 2016",
+      desc:
+        "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
+      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+    }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend / Design", //Insert stack or technology you have experience in
+      progressPercentage: "80%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "50%"
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "80%"
+    }
+  ],
+  displayCodersrank: true // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Full Stack Web Developer",
+      company: "Inventory",
+      companylogo: require("./assets/images/InventoryLogo.png"),
+      date: "December 2020 – Present",
+      desc:
+        "Ecommerce Platform",
+      descBullets: [
+        "Front-end Development over Reactjs Framework",
+        "Firebase serverless back-end"
+      ]
+    },
+    {
+      role: "Full Stack Web Developer",
+      company: "Question",
+      companylogo: require("./assets/images/QuestionLogo.png"),
+      date: "July 2019 – Decemeber 2020",
+      desc:
+        "Question and Answer Platform",
+      descBullets: [
+        "For front-end development JavaScript, HTML, CSS, and bootstrap were used",
+        "The back-end was built using Node.js, Express sever, MongoDB Atlas, JWT and RestfulAPI",
+        "Application is been deployed on Heroku, so it take time to load for first time"
+      ]
+    },
+    {
+      role: "Co-Founder and Full Stack Developer ",
+      company: "Serverom",
+      companylogo: require("./assets/images/ServeromLogo.png"),
+      date: "September 2019 – June 2020",
+      desc:
+        "Managed Cloud Hosting Platform",
+      descBullets: [
+        "I was heavily involved in the creation of the front-end modules.",
+        "Front-end was developed entirely over Embedded JavaScript templating Engine",
+        "The back-end was built using Node.js, Express server, PostgreSQL as a database"
+      ]
+    }
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
+  githubUserName: "theMubashar", // Change to your github username to view your profile in Contact Section.
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Main Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: require("./assets/images/ServeromLogo.png"),
+      projectName: "Serverom",
+      projectDesc: "Managed Cloud Hosting Platform",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "Serverom.com"
+        }
+      ]
+    },
+    {
+      image: require("./assets/images/InventoryLogo.png"),
+      projectName: "Inventory",
+      projectDesc: "Ecommerce Platform",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://Inventory-a761c.web.app"
+        }
+        //  you can add extra buttons here.
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "Google Code-In Finalist",
+      subtitle:
+        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+      image: require("./assets/images/codeInLogo.webp"),
+      footerLink: [
+        {
+          name: "Certification",
+          url:
+            ""
+        },
+        {
+          name: "Award Letter",
+          url:
+            ""
+        },
+        {
+          name: "Google Code-in Blog",
+          url:
+            ""
+        }
+      ]
+    },
+    {
+      title: "Google Assistant Action",
+      subtitle:
+        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+      image: require("./assets/images/googleAssistantLogo.webp"),
+      footerLink: [
+        {
+          name: "View Google Assistant Action",
+          url:
+            ""
+        }
+      ]
+    },
+
+    {
+      title: "PWA Web App Developer",
+      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+      image: require("./assets/images/pwaLogo.webp"),
+      footerLink: [
+        {name: "Certification", url: ""},
+        {
+          name: "Final Project",
+          url: ""
+        }
+      ]
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+
+  blogs: [
+    {
+      url:
+        "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description:
+        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT is The Best?",
+      description:
+        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    ""
+  ),
+
+  talks: [
+    {
+      title: "",
+      subtitle: "",
+      slides_url: "",
+      event_url: ""
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    ""
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Discuss a project or just want to say hi? My Inbox is open for all.",
+  number: "+92 3028652470",
+  email_address: "mubasharhussain404@outlook.com"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "davidrakosi_", //Replace "twitter" with your twitter username without @
+  display: false // Set true to display this section, defaults to false
+};
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails
+};
